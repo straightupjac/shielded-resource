bfb489ebdfd3c6b179e503e22b372e0a99a93ccc
straightupjac <jdc.jaclyn@gmail.com>
2022-04-09 15:51:46 -0400

    help resource page

---
+++ pages/details/ecosystem.tsx
@@ -0,0 +1,30 @@
+import QuickLeaveButton from '@components/quickLeaveButton'
+import { Container, Typography } from '@mui/material'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Ecosystem: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Fresh Meal - contact</title>
+        <meta name="description" content="Quick and easy recipes for the modern family." />
+        <link rel="icon" type="image/png" href="small-logo.png" />
+      </Head>
+      <Container>
+        <main>
+          <QuickLeaveButton />
+          <Typography variant="h1">
+            Privacy Resources
+          </Typography>
+          <Typography variant="body1">
+            Email{' '}
+            <code>contact@freshmeal.com</code> :)
+          </Typography>
+        </main>
+      </Container>
+    </>
+  )
+}
+
+export default Ecosystem;
--- pages/details/index.tsx
+++ pages/details/index.tsx
@@ -3,49 +3,50 @@ import Head from 'next/head'
 import styles from '@styles/details.module.css'
 import QuickLeaveButton from '@components/quickLeaveButton'
 import Link from 'next/link'
+import { Container } from '@mui/material'
 
 const helpResources: NextPage = () => {
 
     return (
-
         <div className={styles.container}>
             <Head>
                 <title>Fresh Meal</title>
                 <meta name="description" content="Quick and easy recipes for the modern family." />
                 <link rel="icon" type="image/png" href="small-logo.png" />
             </Head>
+            <Container>
+                <main className={styles.main}>
+                    <QuickLeaveButton />
+                    <h1>Help Resources</h1>
+                    <div className={styles.grid}>
+                        <Link passHref href='/details/privacy'>
+                            <div className={styles.card}>
+                                <h2>Privacy Resources &rarr;</h2>
+                                <p>Info yay.</p>
+                            </div>
+                        </Link>
 
-            <main className={styles.main}>
-                <span className={styles.quick_leave_button_container}> <QuickLeaveButton /> </span>
-                <h1>Help Resources</h1>
-                <div className={styles.grid}>
-                    <Link passHref href='/'>
-                        <div className={styles.card}>
-                            <h2>Privacy Resources &rarr;</h2>
-                            <p>Info yay.</p>
-                        </div>
-                    </Link>
-
-                    <Link passHref href='/'>
-                        <div className={styles.card}>
-                            <h2>Ethical Design Plan &rarr;</h2>
-                            <p>Info yay.</p>
-                        </div>
-                    </Link>
-                    <Link passHref href='/'>
-                        <div className={styles.card}>
-                            <h2>Manifesto &rarr;</h2>
-                            <p>Info yay.</p>
-                        </div>
-                    </Link>
-                    <Link passHref href='/'>
-                        <div className={styles.card}>
-                            <h2>Ecosystem Research &rarr;</h2>
-                            <p>Info yay.</p>
-                        </div>
-                    </Link>
-                </div>
-            </main>
+                        <Link passHref href='/details/ethical-design'>
+                            <div className={styles.card}>
+                                <h2>Ethical Design Plan &rarr;</h2>
+                                <p>Info yay.</p>
+                            </div>
+                        </Link>
+                        <Link passHref href='/details/manifesto'>
+                            <div className={styles.card}>
+                                <h2>Manifesto &rarr;</h2>
+                                <p>Info yay.</p>
+                            </div>
+                        </Link>
+                        <Link passHref href='/details/ecosystem'>
+                            <div className={styles.card}>
+                                <h2>Ecosystem Research &rarr;</h2>
+                                <p>Info yay.</p>
+                            </div>
+                        </Link>
+                    </div>
+                </main>
+            </Container>
         </div>
     )
 }
